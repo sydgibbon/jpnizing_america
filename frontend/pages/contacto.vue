26fa7390ad8afbcc804d1ab3cd9b8289db9f046d
sydgibbon <57300925+sydgibbon@users.noreply.github.com>
2024-11-03 11:47:28 -0300

    wave

--- frontend/pages/contacto.vue
+++ frontend/pages/contacto.vue
@@ -1,50 +1,59 @@
 <template>
-  <div class="min-h-screen">
-    <!-- Hero Section -->
-    <div class="relative bg-red-500">
-      <span class="absolute inset-0">
-        <span class="w-full h-full bg-cover bg-center block opacity-10" style="background-image: url('https://dokidokijapan.com/wp-content/uploads/2023/11/day_care_bg_02.png')"></span>
-      </span>
+    <div class="">
+        <!-- Hero Section -->
+        <div class="relative bg-red-500 z-10">
+            <span class="absolute inset-0">
+                <span class="w-full h-full bg-cover bg-center block opacity-10"
+                    style="background-image: url('https://dokidokijapan.com/wp-content/uploads/2023/11/day_care_bg_02.png')"></span>
+            </span>
 
-      <section class="relative py-20 px-4">
-        <div class="max-w-4xl mx-auto text-center">
-          <div class="space-y-6">
-            <h1 class="text-5xl md:text-6xl lg:text-7xl font-bold text-white">Contáctanos</h1>
-            <div class="max-w-2xl mx-auto">
-              <p class="text-lg text-white/90">Ayudamos a personas de todo el mundo a hablar Japonés y descubrir su hermosa cultura.</p>
-            </div>
-            <a href="https://wa.me/+541125903475" 
-               class="inline-block bg-black text-white px-8 py-4 rounded-lg hover:bg-gray-800 transition
+            <section class="pt-20 pb-10 mb-20 px-4 ">
+                <div class="max-w-4xl mx-auto text-center">
+                    <div class="space-y-6">
+                        <h1 class="text-5xl md:text-6xl lg:text-7xl font-bold text-white">Contáctanos</h1>
+                        <div class="max-w-2xl mx-auto">
+                            <p class="text-lg text-white/90">Ayudamos a personas de todo el mundo a hablar Japonés y
+                                descubrir su hermosa cultura.</p>
+                        </div>
+                        <a href="https://wa.me/+541125903475" class="inline-block bg-lime-500 text-white px-8 py-4 rounded-lg hover:bg-lime-400 transition
                       text-lg md:text-xl font-bold tracking-wide uppercase 
                       transform hover:scale-105 hover:shadow-lg">
-              Enviar Mensaje
-            </a>
-          </div>
+                            Enviar Mensaje
+                        </a>
+                    </div>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-
-    <!-- Info Section -->
-    <div class="bg-gray-50 py-16 px-4">
-      <div class="max-w-6xl mx-auto grid md:grid-cols-2 gap-12">
-        <div class="space-y-8">
-          <div>
-            <h4 class="text-2xl font-bold text-red-500 mb-4">Contacto</h4>
-            <div class="space-y-4">
-              <p class="hover:text-red-500 transition"><a href="https://wa.me/+5493513168034">+54 9 351 316-8034</a></p>
-              <p class="text-gray-600">hello@antosensei.com</p>
+                
+        <!-- Wave Divider -->
+        <div class="hidden lg:block absolute top-[300px] w-screen z-0">
+            <div class="">
+                <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 390" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><path d="M 0,400 L 0,150 C 84.07142857142858,192.96428571428572 168.14285714285717,235.92857142857142 294,215 C 419.85714285714283,194.07142857142858 587.4999999999999,109.24999999999999 715,111 C 842.5000000000001,112.75000000000001 929.8571428571429,201.07142857142858 1044,222 C 1158.142857142857,242.92857142857142 1299.0714285714284,196.46428571428572 1440,150 L 1440,400 L 0,400 Z" stroke="none" stroke-width="0" fill="#ef4444" fill-opacity="1" class="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 200)"></path></svg>
             </div>
-          </div>
         </div>
 
-        <div>
-          <h4 class="text-2xl font-bold text-red-500 mb-4">Horario</h4>
-          <div class="space-y-2">
-            <p class="text-gray-600">Lunes a Sabado</p>
-            <p class="text-gray-600">7:00am – 8:00pm</p>
-          </div>
+        <!-- Info Section -->
+        <div class="bg-gray-50 py-16 px-20">
+            <div class="max-w-6xl mx-auto grid md:grid-cols-2 gap-12">
+                <div class="space-y-8">
+                    <div>
+                        <h4 class="text-2xl font-bold text-red-500 mb-4">Contacto</h4>
+                        <div class="space-y-4">
+                            <p class="hover:text-red-500 transition"><a href="https://wa.me/+5493513168034">+54 9 351
+                                    316-8034</a></p>
+                            <p class="text-gray-600">hello@antosensei.com</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <h4 class="text-2xl font-bold text-red-500 mb-4">Horario</h4>
+                    <div class="space-y-4">
+                        <p class="text-gray-600">Lunes a Sabado</p>
+                        <p class="text-gray-600">7:00am – 8:00pm</p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
